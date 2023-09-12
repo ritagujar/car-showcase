9f947b6a427896c9ef7f437437f037fa1342da73
ritagujar <ritagujar14@gmail.com>
2023-09-13 00:03:38 +0530

    Designed Hero Section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { customButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: customButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1">{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
